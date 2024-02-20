a3d329e1e9da8ed76900cd5889cc367089c5a29a
Aaron <8454621+sinwailam193@users.noreply.github.com>
2024-02-20 19:03:32 +0800

    Added UI board

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
@@ -121,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+APPEND_SLASH = False
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'utils.exceptions.custom_exception_handler'
+}
---
+++ frontend/pages/browse.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="grid md:grid-cols-4 gap-3 py-10 px-6">
+        <div class="md:col-span-1 p-6 bg-teal-700 rounded-xl">
+            <div class="flex space-x-4">
+                <input
+                    type="search"
+                    placeholder="Find a job..."
+                    class="w-full px-6 py-4 rounded-xl"
+                />
+                <button class="px-6 py-4 bg-teal-900 text-white rounded-xl">
+                    <SearchIcon />
+                </button>
+            </div>
+            <hr class="my-6 opacity-30" />
+            <h3 class="mt-6 text-xl text-white">Categories</h3>
+        </div>
+        <div class="md:col-span-3">Right</div>
+    </div>
+</template>
+
+<script setup></script>
