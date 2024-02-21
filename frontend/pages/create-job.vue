2c343b0302db25b3b4e64cc8f30b98a148bd3a77
Aaron <8454621+sinwailam193@users.noreply.github.com>
2024-02-21 20:15:59 +0800

    Adding loging and sigup page

---
+++ frontend/pages/create-job.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="py-10 px-6">
+        <h1 class="mb-6 text-2xl">Create job</h1>
+
+        <form class="space-y-4">
+            <div>
+                <label>Category</label>
+
+                <select
+                    class="w-full mt-2 p-4 rounded-xl bg-gray-100 after:right-10 after:absolute"
+                >
+                    <option value="">Select category</option>
+                    <option value="dummy1">Category 1</option>
+                    <option value="dummy2">Category 2</option>
+                </select>
+            </div>
+
+            <div>
+                <label>Title</label>
+                <input
+                    type="text"
+                    class="w-full mt-2 p-4 rounded-xl bg-gray-100"
+                />
+            </div>
+
+            <div>
+                <label>Description</label>
+                <textarea class="w-full mt-2 p-4 rounded-xl bg-gray-100" />
+            </div>
+
+            <div>
+                <label>Salary</label>
+                <input
+                    type="text"
+                    class="w-full mt-2 p-4 rounded-xl bg-gray-100"
+                />
+            </div>
+
+            <div>
+                <label>location</label>
+                <input
+                    type="text"
+                    class="w-full mt-2 p-4 rounded-xl bg-gray-100"
+                />
+            </div>
+
+            <div>
+                <label>Company name</label>
+                <input
+                    type="text"
+                    class="w-full mt-2 p-4 rounded-xl bg-gray-100"
+                />
+            </div>
+
+            <div>
+                <label>Company location</label>
+                <input
+                    type="text"
+                    class="w-full mt-2 p-4 rounded-xl bg-gray-100"
+                />
+            </div>
+
+            <div>
+                <label>Company email</label>
+                <input
+                    type="text"
+                    class="w-full mt-2 p-4 rounded-xl bg-gray-100"
+                />
+            </div>
+
+            <button class="py-4 px-6 bg-teal-700 text-white rounded-xl">
+                Submit
+            </button>
+        </form>
+    </div>
+</template>
+
+<script setup></script>
